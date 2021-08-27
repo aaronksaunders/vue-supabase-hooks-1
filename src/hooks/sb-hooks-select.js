84df9402252d2de9caf69e9f62a554a9158dc656
Aaron Saunders <aaronksaunders@gmail.com>
2021-08-26 23:20:36 -0400

    cleanup

--- src/hooks/sb-hooks-select.js
+++ src/hooks/sb-hooks-select.js
@@ -1,15 +1,23 @@
 import { ref } from "vue";
 import useClient from "./sb-hooks";
 
+/**
+ * 
+ * @param {*} table - table to execute query against
+ * @param {*} columns - columns to return
+ * @param {*} auto - if true, run query at startup
+ * @returns 
+ */
 export default function (table, columns = "*", auto = true) {
   const error = ref(null);
   const data = ref([]);
   const loading = ref(false);
 
+  // get supabase client
   const client = useClient();
 
   /**
-   * 
+   * this is the function that does the actual query
    */
   const doQuery = async () => {
     loading.value = true;
@@ -22,14 +30,17 @@ export default function (table, columns = "*", auto = true) {
     error.value = qError;
   };
 
+  // if auto flag is set, run a startup
   if (auto) {
     doQuery();
   }
 
   return {
+    // reactive variables...
     error,
     data,
     loading,
+    // function to call to execute query
     doQuery,
   };
 }
